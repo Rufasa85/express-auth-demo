8025f8c6d7c644cf959b4935a6d81c020d4cc590
Joseph Rehfuss <josephrehfuss@L10241-FVFXVBL0HV22.domain>
2020-04-21 11:47:09 -0700

    working?

---
+++ index.js
@@ -0,0 +1,48 @@
+var express = require('express');
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+require("dotenv").config();
+// var allRoutes = require('./controllers');
+//new coomment
+// Requiring our models for syncing
+var db = require('./models');
+const session = require("express-session");
+var SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    store: new SequelizeStore({
+        db: db.sequelize
+    }),
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 7200000
+    }
+}))
+
+// Static directory
+// app.use(express.static('public'));
+
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// app.use('/',allRoutes);
+
+const authRoutes = require("./controllers/authController");
+app.use(authRoutes);
+
+db.sequelize.sync({ force: false }).then(function () {
+    app.listen(PORT, function () {
+        console.log('App listening on PORT ' + PORT);
+    });
+}).catch(err => {
+    throw err;
+});
